26b4a006df0a3da40b28090d4873b7d33b3a742d
Nidhi <nidhi02@sap.com>
2024-01-14 21:33:03 +0530

    Notes and code for episode 3

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World From React!");
+import React from "react";
+import ReactDOM  from "react-dom/client";
+const TitleComponent = () => <h1 className="head">Namaste React Using JSX</h1>
+//Component Composition
+const HeadingComponent = () => {
+    return(
+    <div className="div">
+       <TitleComponent/>
+      <h1 className="headName">Heading Component</h1>;
+    </div>);
+
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
